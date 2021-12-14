8be63691931d4292c9f4d30600dc4d4c36d0d708
Krishanu Dutta <hrickkrishanudutta@gmail.com>
2021-12-15 01:17:07 +0530

    update: js file

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const url = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
 var arrayOfObject = [];
 fetch(url).then(res => res.json())
-.then(data => {
-    data.map(obj => console.log(obj))
-}
+.then(data => arrayOfObject.push(...data)
     );
-
+function findMatches(wordToMatch, arrayOfObject){
+    return arrayOfObject.fi
+}
 
