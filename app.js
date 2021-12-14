1fec8b715c67f100bbf26854c78b55e2c23a4b28
Krishanu Dutta <hrickkrishanudutta@gmail.com>
2021-12-15 03:08:58 +0530

    feat: suggestions, hover effect

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
 const url = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
-var arrayOfObject = [];
+var cities = [];
 fetch(url).then(res => res.json())
-.then(data => arrayOfObject.push(...data)
+.then(data => cities.push(...data)
     );
-function findMatches(wordToMatch, arrayOfObject){
-    return arrayOfObject.fi
+function findMatches(wordToMatch, cities){
+    return cities.filter( place => {
+        const regex = new RegExp( wordToMatch, 'gi' );
+        return place.city.match(regex) || place.state.match(regex)
+    } );
 }
+function displayMatches(){
+    const matchedArray = findMatches(this.value , cities);
+    const html = matchedArray.map(place =>{
+        return `
+        <li>
+            <span class="name">${place.city}, ${place.state}</span>
+            <span class="population">${place.population}</span>
+        </li>
+        `;
+    }).join("") ;
+
+    suggestions.innerHTML = html;
+}
+const searchInput = document.querySelector('.search');
+const suggestions = document.querySelector('.suggestions');
+searchInput.addEventListener("keyup",displayMatches);
+
 
